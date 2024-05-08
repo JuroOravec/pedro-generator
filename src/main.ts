c80daabc172b9547091dd5187b1bfd5d02b4c943
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-05-08 20:01:56 +0200

    refactor: add telemetry

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,14 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import mixpanel from 'mixpanel-browser'
 
 import App from './App.vue'
+
+mixpanel.init('9ee02d4a064c2071b581e26edcf4eb90', {
+  track_pageview: true,
+  persistence: 'localStorage'
+})
 
 const app = createApp(App)
 
